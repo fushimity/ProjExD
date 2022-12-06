96f34a9247f0c49916854c328e752af21fe74659
fushimity <fushimity@edu.teu.ac.jp>
2022-12-06 14:28:15 +0900

    練習3-3

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="fig/8.png")
+    canvas.create_image(cx, cy, image=tori, tag="kokaton")
     root.mainloop()
-
