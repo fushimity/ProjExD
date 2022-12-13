0aea0a024a6b2ace6c5a12e2916d192cb127adec
fushimity <fushimity@edu.teu.ac.jp>
2022-12-13 15:25:42 +0900

    練習4-8

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -78,6 +78,11 @@ def main():
         yoko, tate = check_bound(bomb_rct, scrn_rct)
         vx *= yoko
         vy *= tate
+
+        # 練習８
+        if tori_rct.colliderect(bomb_rct):
+            return
+
         pg.display.update()
         clock.tick(1000)
 
